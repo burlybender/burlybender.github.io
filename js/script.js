43369214f8655f7d2e4a84f7203b64b402589720
burlybender <67209092+burlybender@users.noreply.github.com>
2020-06-29 12:47:45 -0400

    script and style files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var rootbutton = document.getElementById("root");
+
+rootbutton.onclick = function(){
+    var callelement = document.getElementById("calltoactionheading");
+    callelement.style.display = "block";
+}
